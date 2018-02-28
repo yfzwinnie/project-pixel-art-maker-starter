ed5c2d90d69df1467f54885fa015591ee85a92e9
Winnie <yang.feng.zhang@avanade.com>
2018-02-28 15:09:57 -0800

    Finished v2 of Pixel Art Maker

--- designs.js
+++ designs.js
@@ -1,10 +1,31 @@
-// Select color input
-// Select size input
+function makeGrid(event) {
+	event.preventDefault();
+	const height = $("#inputHeight").val();
+	const width = $("#inputWidth").val();
+	const canvas = $("#pixelCanvas");
 
-// When size is submitted by the user, call makeGrid()
+	canvas.empty();
 
-function makeGrid() {
-
-// Your code goes here!
-
+	for (let row = 0; row <= height; row++) {
+		canvas.append(`<tr class='row${row}'></tr>`);
+		for (let col = 1; col <= width; col++) {
+			$(`.row${row}`).append("<td class='grid'></td>");
+		}
+	}
 }
+
+let colorSelected = "#000";
+function colorPick() {
+	colorSelected = $("#colorPicker").val();
+}
+
+$("#pixelCanvas").mousemove(function drawGrid(e) {
+		console.log(e.which);
+	if (e.which === 1) {
+			$(e.target).css("background-color", colorSelected);
+		}
+	}
+);
+
+$("#sizePicker").submit(makeGrid);
+$("#colorPicker").change(colorPick);
